9efbbdc87728d3479391432c03af57965d167be7
Abhishek Singh <rohitgurjar.khari@gmail.com>
2024-05-11 21:21:36 +0530

    making login page with ejs and connecting mongodb with index file

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express= require("express");
- const app= express();
+const mongoose = require('mongoose');
+const app= express();
+mongoose.connect("mongodb://localhost:27017/userdata");
+app.use(express.json());
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/signup",(req,res)=>{
+    res.render("signup");
+})
+
+app.listen(5000);
  
