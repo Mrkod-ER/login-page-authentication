6bcae8afb523f5c695b760c1dcbebc60c367e550
Abhishek Singh <rohitgurjar.khari@gmail.com>
2024-05-12 19:21:00 +0530

    making signup page and connecting it to index.js

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express= require("express");
+const path = require("path");
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 const app= express();
 mongoose.connect("mongodb://localhost:27017/userdata");
+
+
 app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.get("/signup",(req,res)=>{
     res.render("signup");
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login");
 })
 
 app.listen(5000);
